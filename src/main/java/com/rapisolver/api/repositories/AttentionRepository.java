a301afc117a4f9a3d6661aa0ffd6ca04cb4f0323
Jose Matos <Jose.matos.contractor@bbva.com>
2021-05-06 11:08:06 -0500

    agregando query de frecuencia

--- src/main/java/com/rapisolver/api/repositories/AttentionRepository.java
+++ src/main/java/com/rapisolver/api/repositories/AttentionRepository.java
@@ -4,13 +4,19 @@ import com.rapisolver.api.dtos.AttentionDTO;
 import com.rapisolver.api.dtos.CreateAttentionDTO;
 import com.rapisolver.api.entities.Attention;
 import com.rapisolver.api.entities.Category;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AttentionRepository extends JpaRepository<Attention,Long> {
-
-
+public interface AttentionRepository extends JpaRepository<Attention, Long> {
+  @Query(
+    value = "SELECT A.NAME" +
+    "FROM ATTENTIONS A INNER JOIN SUPPLIER_ATTENTIONS B ON A.ID = B.ATTENTION_ID INNER JOIN RESERVATIONS C ON B.ID = B.USER_SUPPLIER_ID WHERE C.STATUS = 2 GROUP BY A.NAME",
+    nativeQuery = true
+  )
+  List<Attention> getAllAttentionsByDelivered();
 }
+// ,COUNT(*) AS CANTIDAD
--- src/main/java/com/rapisolver/api/services/AttentionService.java
+++ src/main/java/com/rapisolver/api/services/AttentionService.java
@@ -2,12 +2,14 @@ package com.rapisolver.api.services;
 
 import com.rapisolver.api.dtos.*;
 import com.rapisolver.api.exceptions.RapisolverException;
-
 import java.util.List;
 
 public interface AttentionService {
-    List<AttentionDTO> findAttentions() throws RapisolverException;
-    AttentionDTO findById(Long attentionId) throws RapisolverException;
-    AttentionDTO createAttention(CreateAttentionDTO createAttentionDTO) throws RapisolverException;
-    AttentionDTO updateAttention(Long id, CreateAttentionDTO createAttentionDTO) throws RapisolverException;
+  List<AttentionDTO> findAttentions() throws RapisolverException;
+  AttentionDTO findById(Long attentionId) throws RapisolverException;
+  AttentionDTO createAttention(CreateAttentionDTO createAttentionDTO)
+    throws RapisolverException;
+  AttentionDTO updateAttention(Long id, CreateAttentionDTO createAttentionDTO)
+    throws RapisolverException;
+  List<AttentionDTO> getAllAttentionByDelivered() throws RapisolverException;
 }
